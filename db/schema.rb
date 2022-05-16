14e96f87c43d80c3205da079a9a74f6b2348160a
Mohan Salvi <mohan.salvi@vts.com>
2022-05-16 16:13:49 +0530

    database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_05_16_091245) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "author_name"
@@ -21,7 +24,7 @@ ActiveRecord::Schema.define(version: 2022_05_16_091245) do
 
   create_table "comments", force: :cascade do |t|
     t.string "title"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_comments_on_user_id"
@@ -39,7 +42,7 @@ ActiveRecord::Schema.define(version: 2022_05_16_091245) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "description"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
